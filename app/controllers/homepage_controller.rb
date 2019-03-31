6d73692aa5aeb3b2385a54fe544fc44853f48cee
Hayden Brown <hbrown10@binghamton.edu>
2019-03-31 23:36:39 +0000

    Clean up homepage controller and view files

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,17 +7,12 @@ class HomepageController < ApplicationController
   def search
     search_bar = params[:search_bar] || session[:search_bar]
     search_type = params[:search_type] || session[:search_type]
-    case search_type
-    when 'food'
-      redirect_value = 'food'
-    when 'restaurant'
-      redirect_value = 'restaurant'
-    end
     if search_bar.empty?
       redirect_to '/'
     else
+      session[:search_type] = search_type
       session[:search_bar] = search_bar
-      redirect_to controller: 'search', action: redirect_value, search_bar: search_bar
+      redirect_to controller: 'search', action: search_type, search_bar: search_bar
     end
   end
   
