975297aec8c4055c1c2e169c2f17b60402754eb8
Max <mnussba2@binghamton.edu>
2019-04-10 16:18:15 +0000

    generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/failure'
+
+  get 'sessions/destroy'
+
   get 'search' => 'homepage#search'
   get 'results/food' => 'search#food'
   get 'results/restaurant' => 'search#restaurant'
