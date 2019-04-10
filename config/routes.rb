e88ef8a7e928a01a7f16940774330e4116c3f44c
Max <mnussba2@binghamton.edu>
2019-04-10 16:23:16 +0000

    add updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/failure'
-
-  get 'sessions/destroy'
-
+  get '/login', :to => 'sessions#new', :as => 'login'
+  match 'auth/:provider/callback', :to => 'sessions#create', :via => [:get, :post]
+  match 'auth/failure', :to => 'sessions#failure', :via => [:get, :post]
+  get 'sessions/destroy', :as => 'logout'
   get 'search' => 'homepage#search'
   get 'results/food' => 'search#food'
   get 'results/restaurant' => 'search#restaurant'
